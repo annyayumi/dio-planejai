cc9cd6606ccf4d74c9be15784205a93e9c9e5f2e
Anny Ayumi <anny.iogi@ammya.com.br>
2026-08-10 16:56:09 -0300

    feat: cabeçalho e menu de navegação

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,14 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom'
+
+import { RootLayout } from '@/components/shared/layout/RootLayout'
 
 export const router = createBrowserRouter([
   {
+    element: <RootLayout />,
     children: [
       {
         path: '/',
-        element: (
-          <>
-          <h1>Formulário de Simulação</h1>
-          </>
-        )
+        element: <h1>Formulário de Simulação</h1>,
       },
       {
         path: '/resultado',
