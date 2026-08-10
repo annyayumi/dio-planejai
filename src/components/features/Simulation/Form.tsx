7f0f86aceff26d6b0dd7c36aca714ad68798b977
Anny Ayumi <anny.iogi@ammya.com.br>
2026-08-10 19:41:19 -0300

    feat: Interface do Formulário de Simulação

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,9 +1,13 @@
+import { PiggyBank } from 'lucide-react'
+
 import { StepProgress } from './Progress';
+import { FormStep } from './FormStep';
 
 export const SimulationForm = () => {
   return (
       <>  
       <StepProgress currentStep={1} totalSteps={6} />
+      <FormStep icon={PiggyBank} title="Renda Mensal Bruta" question="Quanto é depositado na sua conta todo mês (somando todas as fontes)?" inputProps={{type: 'text', placeholder: 'Ex: R$ 3.000,00', prefix: 'R$'}}/>
       </>
   )
 }
